c88fd391b96a9e006ea851e4633dd8c9edaade80
mattiavisona <mattiav2706@gmail.com>
2023-04-25 23:33:37 +0200

    update

--- index.php
+++ index.php
@@ -5,12 +5,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Exercise</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Strong Password Exercise</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Strong Password Exercise</h1>
+
+    <style>
+        body {
+            background-color: beige;
+        }
+
+        .btn-primary {
+            text-align: center;
+            padding: 15px 25px;
+        }
+    </style>
+
+    <div class="container pt-2">
+        <h1>Password Generator</h1>
+
+        <form action="index.php" method="GET">
+
+            <div class="mb-3">
+                <label for="passwordLength" class="form-label">Password Length :</label>
+                <input name="passwordLength" id="passwordLength" type="number" min="4" step="1" placeholder="Length">
+
+                <hr>
+
+                <button type="submit" class="btn btn-primary">Start</button>
+            </div>
+
+        </form>
+
+        <hr>
+
+        <?php
+
+        if ($generatedPassword != "") {
+
+        ?>
+
+            <h2>Password generata:</h2>
+            <h3><?= $_GET['passwordLength'] ?> caratteri</h3>
+
+            <pre class="text-danger"><?= $generatedPassword ?></pre>
+
+        <?php
+
+        }
+
+
+        ?>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
